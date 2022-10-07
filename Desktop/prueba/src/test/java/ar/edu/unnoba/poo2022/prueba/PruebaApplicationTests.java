e6068d7e1b2c4c6bd9c59f25ad3cb5fcc761dda7
gabriel <gfiori@comunidad.unnoba.edu.ar>
2022-10-07 20:34:07 -0300

    agrego gitignore que lo modifique

---
+++ Desktop/prueba/src/test/java/ar/edu/unnoba/poo2022/prueba/PruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2022.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
